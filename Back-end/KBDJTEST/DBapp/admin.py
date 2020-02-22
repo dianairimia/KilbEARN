19cde9ee795a89c67c59c7cbee3baad82262ffca
u07236av <alan.vincent@Student.manchester.ac.uk>
2020-02-22 16:02:01 +0000

    hey

--- Back-end/KBDJTEST/DBapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from DBapp.models import Main, Friends, Details
-
-admin.site.register(Main)
-admin.site.register(Friends)
-admin.site.register(Details)
--- Back-end/KBDJTEST/DBapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class Login(ModelForm):
-    username=forms.CharField()
-    password=forms.CharField()
-    email=forms.CharField()
