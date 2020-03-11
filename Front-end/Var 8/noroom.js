ca440bdd379bfc6f9302c7c331fbc6f7461e66fe
f62657di <diana-cristina.irimia@student.manchester.ac.uk>
2020-03-11 14:48:39 +0000

    alert boxes and edited stuff KBFIX has to be updated

---
+++ Front-end/Var 8/noroom.js
@@ -0,0 +1,9 @@
+var clicked[] = [0, 0, 0, 0, 0, 0, 0];
+var buttons[] = []
+function selectedIcon(name) {
+  if (clicked.includes(1))
+  {
+      var pos = clicked.indexOf(1, 0);
+  }
+  name.style.border = "2px solid white";
+}
