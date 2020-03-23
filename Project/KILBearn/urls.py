b691b0f89cda07d7bbe772fa1f388d192d3e4e11
u07236av <alan.vincent@Student.manchester.ac.uk>
2020-03-23 16:28:12 +0000

    Should be done.

---
+++ Project/KILBearn/urls.py
@@ -0,0 +1,24 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from DBapp import views
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^DB/', include('DBapp.urls')),
+    url(r'^login', views.LoginPage, name='index'),
+    url(r'^computer', views.computer, name='index'),
+    url(r'^instructions', views.instructions, name='index'),
+    url(r'^noroom', views.noroom, name='index'),
+    url(r'^people', views.people, name='index'),
+    url(r'^play', views.play, name='index'),
+    url(r'^profile', views.profile, name='index'),
+    url(r'^register', views.register, name='index'),
+    url(r'^settings', views.settings, name='index'),
+    url(r'^welcome', views.welcome, name='index'),
+    url(r'^mainmenu', views.mainmenu, name='index'),
+    url(r'^board', views.board, name='index'),
+    url(r'admin/', admin.site.urls),
+    path('chat/', views.chat, name='index'),
+    path('kbconnect/', views.room, name='room'),
+]
