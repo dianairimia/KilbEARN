f92935bdbd5a94d16de286582bc62b928c07b0b9
u07236av <alan.vincent@Student.manchester.ac.uk>
2020-03-23 14:39:37 +0000

    Message bugs on whatsapp

---
+++ KB_FINAL_actual/KB_FINAL_AR/DBapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from DBapp.models import Main, Friends, Details, Icons
+
+admin.site.register(Main)
+admin.site.register(Friends)
+admin.site.register(Details)
+admin.site.register(Icons)
